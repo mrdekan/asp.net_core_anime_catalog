6e6d4dab390b0bc879a6fe49e128c6d57439fa60
mrdekan <yeros05040504@gmail.com>
2023-03-04 21:20:08 +0200

    Created header

---
+++ AnimeCatalog/Controllers/AnimeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeCatalog.Controllers
+{
+    public class AnimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
