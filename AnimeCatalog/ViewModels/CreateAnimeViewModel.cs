31f6d6110eafa7d2af5373c1f2f8fb9c11a74d28
mrdekan <yeros05040504@gmail.com>
2023-03-07 19:56:39 +0200

    Some changes

---
+++ AnimeCatalog/ViewModels/CreateAnimeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnimeCatalog.ViewModels
+{
+	public class CreateAnimeViewModel
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[RegularExpression(@"^[0-9]*$",
+		 ErrorMessage = "Characters are not allowed.")]
+		public string SeasonsAndSeries { get; set; }
+		[Required]
+		public string Tags { get; set; }
+	}
+}
